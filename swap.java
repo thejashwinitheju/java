bdb0e3965635b03bdc0b36882e516b437a49fad6
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-12-27 22:08:43 +0530

    conditional statment

--- swap.java
+++ swap.java
@@ -1,4 +1,6 @@
-class swap {
+//Swap a number
+
+/*class swap {
 
   public static void main(String[] swap){
     int a=10 ,b=20;
@@ -9,7 +11,54 @@ class swap {
   }
   
 
-}
+}*/
 
+
+//check a number divisable by 7 or not
+
+/*public class swap{
+public static void main (String[] args){
+  int digit = 90;
+  if(digit%7==0){
+    System.out.println("7 is divisable");
+  }else{
+    System.out.println("7 is not divisanle");
+  
+  
+  }
+
+}
+}*/
+
+
+
+//check a number is multiple of 3 or not
+
+/*public class swap{
+  public static void main (String[] args){
+    int digit =10;
+    if (digit%3==0){
+      System.out.println("multiple of 3");
+    }else{
+      System.out.println("not multiple of 3");
+    
+    }
+
+  }
+}*/
+
+// check a number is postive or odd
+
+public class swap{
+  public static void main (String[] args){
+    int a=999;
+    if(a%2==0){
+      System.out.println("the given number is postive");
+    }else{
+      System.out.println("the given number is odd");
+    
+    }
+  }
+}
 
 
