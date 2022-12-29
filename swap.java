716cdc133e14fc5c2dbf240e03c9b892b3de10ab
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-12-29 16:11:08 +0530

    conditional statement

--- swap.java
+++ swap.java
@@ -47,18 +47,138 @@ public static void main (String[] args){
   }
 }*/
 
-// check a number is postive or odd
+// check a number is postive or negative
 
-public class swap{
+/*public class swap{
   public static void main (String[] args){
-    int a=999;
-    if(a%2==0){
-      System.out.println("the given number is postive");
+    int i=-2;
+    if(i>0){
+      System.out.println("the given number is positive");
     }else{
-      System.out.println("the given number is odd");
+      System.out.println("the given number is negative");
     
     }
   }
-}
+}*/
+
+
+// check a number is 3-digit or not
+
+/*public class swap{
+  public static void main (String[] arg){
+    int i=999;
+    if (i>99&&i<1000){
+System.out.println("the given number is 3-digit");
+    }else{
+      System.out.println("the given number is not 3-digit");
+
+    }
+
+  }
+}*/
+
+
+
+// check the number is even or odd
+
+/*public class swap{
+  public static void main (String[] arg){
+    int i=10;
+    if (i%2==0){
+System.out.println("the given number is even");
+    }else{
+      System.out.println("the given number is not odd");
+
+    }
+  }
+  }*/
+
+
+  //check the greatest number in given two number
+
+  /*public class swap{
+    public static void main(String[] args){
+      int a=40;
+      int b=50;
+      if (a>b){
+        System.out.println("B is greater then A");
+
+      }else{
+        System.out.println("B is not grater then A");
+
+      }
+
+    }
+  }*/
+
+
+  //check the number greatest number in given three number
+
+  /*public class swap{
+    public static void main(String[] args){
+      int a=20;
+      int b=30;
+      int c=40;
+      
+      switch (a>b||b>c||c>a){
+        case 10 :System.out.println("10");
+        break;
+        case 20:System.out.println("20");
+        break;
+        case 30:System.out.println("30");
+        break;
+        default:System.out.println("not 10,20,30");
+
+      }
+
+    }
+  }*/
+
+
+  // check the greatest in a given three number
+
+/*import java.util.Scanner;
+  public class swap{
+    public static void main(String[] args){
+    int x,y,z;
+    Scanner s = new Scanner(System.in);
+    System.out.println("enter the first number");
+  x=s.nextInt();
+  System.out.println("enter the second number");
+  y=s.nextInt();
+  System.out.println("enter the third number");
+  z=s.nextInt();
+    if (x>y&&x>z){
+      System.out.println("largest number is:+x");
+    }else if (y>z){
+      System.out.println("largest number is:+y");
+    }else{
+      System.out.println("largest number is:+z");
+    }
+    }
+  }*/
+
+
+  // ascending order
+
+  public class swap{
+    public static void main(String[] args){
+      int[] ass = new int[]{5,6,2,4,8};
+      for(int i=0;i<ass.length;i++){
+        System.out.println(ass[i]);
+
+      }
+      for(int i=0;i<ass.length;i++){
+        for (int j=0;j<ass.length;j++){
+
+        }
+      }
+
+      }
+    }
+  
+
+
+
 
 
