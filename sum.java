81cd09f72c28cbe40b931d88865e367134a26e74
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-03 17:49:15 +0530

    sum of natural

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+    public static void main(String[] args) {
+    int i,num=20, sum = 0;
+        for ( i = 1 ; i<=num ; ++i){
+            sum = sum+i;
+            System.out.print(sum + " + ");
+        }
+       
+    }
+}
+
+    
