ac361200dde26f821544462d11bb4cff7c449509
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-04 14:39:28 +0530

    divisable 3,5

---
+++ div235.java
@@ -0,0 +1,25 @@
+public class div235 {
+    public static void main(String[] args){
+       for(int i=1;i<=100;i++){
+        if(i%5==0){
+        System.out.println(i);
+            
+        }
+    }
+        for(int i=1;i<=100;i++){
+            if(i%3==0){
+            
+            System.out.println(i);
+               
+            
+        }
+        
+    }
+ }
+
+}
+
+
+
+
+
