793721c451e7cd616f17f5e6065eb7a2574e5be8
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-05 13:15:05 +0530

    armstrong

---
+++ itarative/amrstrong.java
@@ -0,0 +1,43 @@
+/*package itarative;
+
+import java.util.Scanner;
+
+public class amrstrong {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num =sc.nextInt();
+        int backUP = num;
+        int sum = 0;
+
+        while(num>0){
+            sum = sum + (num%10)*(num%10)*(num%10);
+            num = num/10;
+        }
+        if(sum==backUP){
+            System.out.println("Amrstrong");
+        }else{
+            System.out.println("NOT Amrstrong");
+        }
+    }
+    
+}*/
+
+
+class amrstrong{
+    public static void main(String[] args){
+        int n = n;
+        int temp , digit=0 , last=0 , sum=0;
+        temp=n;
+        while(temp>0){
+            temp=temp/10;
+            digit++;
+        }
+        temp=n;
+        while(temp>0){
+            last=temp%10;
+            sum + =(math.pow(last,digits));
+        }
+
+        }
+    }
+
