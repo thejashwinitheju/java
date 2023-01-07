dd8acc49cf0b51e1ae652938d946ca360f985b15
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-07 12:07:31 +0530

    leap year

---
+++ itarative/leapyear.java
@@ -0,0 +1,14 @@
+//public itarative;
+
+public class leapyear {
+    public static void main(String[] args){
+        int year = 2024;
+        if((year%4==0)&&(year%100!=0)||(year%400==0)){
+            System.out.println("leap year");
+        }else{
+            System.out.println("not leap year");
+        }
+    }
+
+    
+}
