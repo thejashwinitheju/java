408d3d0a3d334e050db2e7e081c7544261382eac
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-13 11:43:50 +0530

    divisable of 7

---
+++ conditional/div7.java
@@ -0,0 +1,14 @@
+package conditional;
+
+public class div7 {
+    public static void main (String[] args){
+        int digit = 90;
+        if(digit%7==0){
+          System.out.println("7 is divisable");
+        }else{
+          System.out.println("7 is not divisanle");
+        
+        
+        }
+    
+    }}
--- swap.java
+++ swap.java
@@ -15,15 +15,7 @@
 //check a number divisable by 7 or not
 
 /*public class swap{
-public static void main (String[] args){
-  int digit = 90;
-  if(digit%7==0){
-    System.out.println("7 is divisable");
-  }else{
-    System.out.println("7 is not divisanle");
-  
-  
-  }
+
 
 }
 }*/
