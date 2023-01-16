d211c40e086dab6fda4bcff38f3b20ad5151d2f7
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-16 09:23:11 +0530

    multiple of 3

---
+++ conditional/multi3.java
@@ -0,0 +1,16 @@
+package conditional;
+
+public class multi3 {
+    public static void main (String[] args){
+      int digit =10;
+      if (digit%3==0){
+        System.out.println("multiple of 3");
+      }else{
+        System.out.println("not multiple of 3");
+      
+      }
+  
+    }
+  }
+    
+
--- swap.java
+++ swap.java
@@ -12,30 +12,13 @@
   }*/
 
 
-//check a number divisable by 7 or not
 
-/*public class swap{
-
-
-}
-}*/
 
 
 
 //check a number is multiple of 3 or not
 
-/*public class swap{
-  public static void main (String[] args){
-    int digit =10;
-    if (digit%3==0){
-      System.out.println("multiple of 3");
-    }else{
-      System.out.println("not multiple of 3");
-    
-    }
-
-  }
-}*/
+/**/
 
 // check a number is postive or negative
 
