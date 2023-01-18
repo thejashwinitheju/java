08b03e1291571ef84c3b3e5ff3853126a5985f84
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-18 10:56:16 +0530

    check num 3digit

---
+++ check3.java
@@ -0,0 +1,15 @@
+public class check3 {
+
+  
+  public static void main (String[] args){
+    int i=-2;
+    if(i>0){
+      System.out.println("the given number is positive");
+    }else{
+      System.out.println("the given number is negative");
+    
+    }
+  }
+
+    
+}
--- swap.java
+++ swap.java
@@ -22,17 +22,7 @@
 
 // check a number is postive or negative
 
-/*public class swap{
-  public static void main (String[] args){
-    int i=-2;
-    if(i>0){
-      System.out.println("the given number is positive");
-    }else{
-      System.out.println("the given number is negative");
-    
-    }
-  }
-}*/
+/
 
 
 // check a number is 3-digit or not
